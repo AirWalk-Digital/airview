7b938a670ee4118bc4e2e64ce18e7d75b05e357a
James Humphreys <BinaryJim@users.noreply.github.com>
2022-03-16 14:46:06 +0000

    Add initial EntryCreator component

--- src/react-query-demo/features/entry-creator/entry-creator.js
+++ src/react-query-demo/features/entry-creator/entry-creator.js
@@ -1,6 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQueryClient } from "react-query";
+import { MetaForm } from "../meta-form";
 
 export function EntryCreator() {
-  return <span>EntryCreator</span>;
+  const setInitialState = () => {
+    return {
+      name: "",
+      collection: "",
+      parent: "",
+      body: "",
+    };
+  };
+
+  const [formData, setFormData] = useState(setInitialState());
+
+  const handleOnFormChange = (event) => {
+    setFormData((prevValue) => ({
+      ...prevValue,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleOnFormReset = (event) => {
+    event.preventDefault();
+
+    setFormData(setInitialState());
+  };
+
+  return (
+    <div>
+      <h3>Create Entry</h3>
+      <div>Error reporter here</div>
+      <hr />
+      <MetaForm
+        formData={formData}
+        onReset={handleOnFormReset}
+        onChange={handleOnFormChange}
+        onSubmit={() => {}}
+      />
+      <hr />
+    </div>
+  );
 }
