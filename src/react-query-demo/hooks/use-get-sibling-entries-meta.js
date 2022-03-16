96b532ed5b11b296a7a413b43e6bba376e6082b1
James Humphreys <BinaryJim@users.noreply.github.com>
2022-03-16 11:57:16 +0000

    Change return values for useGetSiblingEntriesMeta hook

--- src/react-query-demo/hooks/use-get-sibling-entries-meta.js
+++ src/react-query-demo/hooks/use-get-sibling-entries-meta.js
@@ -2,7 +2,15 @@ import { useMemo } from "react";
 import { useGetAllEntriesMeta } from "./use-get-all-entries-meta";
 
 export function useGetSiblingEntriesMeta(entryId) {
-  const { status, data: entriesMeta, error } = useGetAllEntriesMeta();
+  const {
+    isLoading,
+    isError,
+    isSuccess,
+    isIdle,
+    isFetching,
+    data: entriesMeta,
+    error,
+  } = useGetAllEntriesMeta();
 
   const filteredEntries = useMemo(() => {
     if (!entriesMeta || !entryId) return;
@@ -18,21 +26,31 @@ export function useGetSiblingEntriesMeta(entryId) {
     );
   }, [entriesMeta, entryId]);
 
-  if (entriesMeta) {
-    return {
-      status,
-      data: filteredEntries,
-    };
-  }
-
-  if (status === "error") {
-    return {
-      status,
-      message: error.message,
-    };
-  }
-
   return {
-    status,
+    isLoading,
+    isError,
+    isSuccess,
+    isIdle,
+    isFetching,
+    data: filteredEntries,
+    error,
   };
+
+  // if (entriesMeta) {
+  //   return {
+  //     status,
+  //     data: filteredEntries,
+  //   };
+  // }
+
+  // if (status === "error") {
+  //   return {
+  //     status,
+  //     message: error.message,
+  //   };
+  // }
+
+  // return {
+  //   status,
+  // };
 }
