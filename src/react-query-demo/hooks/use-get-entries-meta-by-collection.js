d92155627a920b03c16f8f5b06af95715d9cc8ad
James Humphreys <BinaryJim@users.noreply.github.com>
2022-03-16 11:22:56 +0000

    Change return value for useGetEntriesMetaByCollection

--- src/react-query-demo/hooks/use-get-entries-meta-by-collection.js
+++ src/react-query-demo/hooks/use-get-entries-meta-by-collection.js
@@ -2,7 +2,15 @@ import { useMemo } from "react";
 import { useGetAllEntriesMeta } from "./use-get-all-entries-meta";
 
 export function useGetEntriesMetaByCollection(collection) {
-  const { status, data: entriesMeta, error } = useGetAllEntriesMeta();
+  const {
+    isLoading,
+    isError,
+    isSuccess,
+    isIdle,
+    isFetching,
+    data: entriesMeta,
+    error,
+  } = useGetAllEntriesMeta();
 
   const filteredCollections = useMemo(() => {
     if (!entriesMeta || !collection) return;
@@ -10,21 +18,31 @@ export function useGetEntriesMetaByCollection(collection) {
     return entriesMeta.filter((entry) => entry?.collection === collection);
   }, [entriesMeta, collection]);
 
-  if (entriesMeta) {
-    return {
-      status,
-      data: filteredCollections,
-    };
-  }
-
-  if (status === "error") {
-    return {
-      status,
-      message: error.message,
-    };
-  }
-
   return {
-    status,
+    isLoading,
+    isError,
+    isSuccess,
+    isIdle,
+    isFetching,
+    data: filteredCollections,
+    error,
   };
+
+  // if (entriesMeta) {
+  //   return {
+  //     status,
+  //     data: filteredCollections,
+  //   };
+  // }
+
+  // if (status === "error") {
+  //   return {
+  //     status,
+  //     message: error.message,
+  //   };
+  // }
+
+  // return {
+  //   status,
+  // };
 }
