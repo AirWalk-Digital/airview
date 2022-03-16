29570f4482b81e65c063badf140b0c1d56e79404
James Humphreys <BinaryJim@users.noreply.github.com>
2022-03-16 11:08:17 +0000

    Change return values from useGetCollections hook

--- src/react-query-demo/hooks/use-get-collections.js
+++ src/react-query-demo/hooks/use-get-collections.js
@@ -2,33 +2,26 @@ import { useMemo } from "react";
 import { useGetAllEntriesMeta } from "./use-get-all-entries-meta";
 
 export function useGetCollections() {
-  const { status, data: entriesMeta, error } = useGetAllEntriesMeta();
+  const { isLoading, isError, isSuccess, isIdle, isFetching, data, error } =
+    useGetAllEntriesMeta();
 
   const collections = useMemo(() => {
-    if (!entriesMeta) return;
+    if (!data) return;
 
-    return entriesMeta.reduce((prevValue, currentValue) => {
+    return data.reduce((prevValue, currentValue) => {
       if (prevValue.includes(currentValue?.collection)) return prevValue;
 
       return [...prevValue, currentValue?.collection];
     }, []);
-  }, [entriesMeta]);
-
-  if (entriesMeta) {
-    return {
-      status,
-      data: collections,
-    };
-  }
-
-  if (status === "error") {
-    return {
-      status,
-      message: error.message,
-    };
-  }
+  }, [data]);
 
   return {
-    status,
+    isLoading,
+    isError,
+    isSuccess,
+    isIdle,
+    isFetching,
+    data: collections,
+    error,
   };
 }
