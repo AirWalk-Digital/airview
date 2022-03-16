737f032ac4925fbf9871fd94d55338c9894517f7
James Humphreys <BinaryJim@users.noreply.github.com>
2022-03-16 11:34:23 +0000

    Covered all states for EntrySelector

--- src/react-query-demo/entry-selector.js
+++ src/react-query-demo/entry-selector.js
@@ -2,18 +2,20 @@ import React from "react";
 import { useGetAllEntriesMeta } from "./hooks";
 
 export function EntrySelector({ onChange, value = "", ...otherProps }) {
-  const { status, data: entries } = useGetAllEntriesMeta();
+  const { isLoading, isError, isFetching, data } = useGetAllEntriesMeta();
 
-  if (status === "loading") {
+  if (isLoading || isFetching) {
     return <div>Loading Entries</div>;
   }
 
-  if (!entries) return <div>No Entries</div>;
+  if (isError) return <div>Error fetching Entries</div>;
+
+  if (!data) return <div>No Entries</div>;
 
   return (
     <select value={value} onChange={onChange} {...otherProps}>
       <option value="">Choose an entry...</option>
-      {entries.map((entry) => (
+      {data.map((entry) => (
         <option key={entry.id} value={entry.id}>
           {entry.name}
         </option>
