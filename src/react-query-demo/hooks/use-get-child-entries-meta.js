2fc7125694423bc42cebd134c0e0cdc76327cc99
James Humphreys <BinaryJim@users.noreply.github.com>
2022-03-17 15:05:35 +0000

    Bail on useGetChildEntriesMeta if entryId is not set

--- src/react-query-demo/hooks/use-get-child-entries-meta.js
+++ src/react-query-demo/hooks/use-get-child-entries-meta.js
@@ -1,7 +1,8 @@
 import { useGetAllEntriesMeta } from "./use-get-all-entries-meta";
 
 export function useGetChildEntriesMeta(entryId) {
-  return useGetAllEntriesMeta((entries) =>
-    entries.filter((entry) => entry?.parent === entryId)
-  );
+  return useGetAllEntriesMeta((entries) => {
+    if (!entryId) return;
+    return entries.filter((entry) => entry?.parent === entryId);
+  });
 }
