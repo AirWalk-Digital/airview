fd858ce0d94be7da0b1eb0c65bf2aed9b70d1f34
James Humphreys <BinaryJim@users.noreply.github.com>
2022-03-21 12:03:41 +0000

    Add config

--- src/react-query-demo/config.js
+++ src/react-query-demo/config.js
@@ -3,14 +3,107 @@ export const config = {
     application: {
       name: "Application",
       id: "application",
+      files: [
+        {
+          name: "index.md",
+          fields: [
+            {
+              type: "string",
+              required: true,
+              name: "name",
+              label: "Name",
+              placeholder: "Enter an application name...",
+            },
+            {
+              type: "textarea",
+              required: false,
+              name: "body",
+              label: "Main body",
+              placeholder: "Enter some body content for the application...",
+            },
+          ],
+        },
+      ],
     },
     knowledge: {
       name: "Knowledge",
       id: "knowledge",
+      files: [
+        {
+          name: "index.md",
+          fields: [
+            {
+              type: "string",
+              required: true,
+              name: "name",
+              label: "Name",
+              placeholder: "Enter a name for the document...",
+            },
+            {
+              type: "entry_selector",
+              required: false,
+              name: "parent",
+              label: "Parent",
+              // value_field: "id",
+              // display_field: "name"
+            },
+            {
+              type: "textarea",
+              required: false,
+              name: "body",
+              label: "Main body",
+              placeholder: "Enter some body content for the document...",
+            },
+          ],
+        },
+        {
+          name: "subsection.md",
+          fields: [
+            {
+              type: "textarea",
+              required: false,
+              name: "body",
+              label: "Subsection body",
+              placeholder: "Enter some body content for the subsection...",
+            },
+          ],
+        },
+      ],
     },
     release: {
       name: "Release",
       id: "release",
+      files: [
+        {
+          name: "index.md",
+          fields: [
+            {
+              type: "string",
+              required: true,
+              name: "name",
+              label: "Name",
+              placeholder: "Enter a name for the document...",
+            },
+            {
+              type: "relationship",
+              required: false,
+              name: "parent",
+              label: "Parent",
+              placeholder: "Choose an entry...",
+              collection: "application",
+              value_field: "id",
+              display_field: "name",
+            },
+            {
+              type: "textarea",
+              required: false,
+              name: "body",
+              label: "Main body",
+              placeholder: "Enter some body content for the document...",
+            },
+          ],
+        },
+      ],
     },
   },
 };
