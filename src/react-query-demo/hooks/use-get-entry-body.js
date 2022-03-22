e52ed2d71cabe93dd10fe0461464e50629113cc2
Mark Cuckson <59055349+mcuckson@users.noreply.github.com>
2022-03-22 11:02:53 +0000

    notes

--- src/react-query-demo/hooks/use-get-entry-body.js
+++ src/react-query-demo/hooks/use-get-entry-body.js
@@ -5,11 +5,20 @@ import { useGetEntryMeta } from "../hooks";
 export function useGetEntryBody(entryId) {
   const { data: entryMeta } = useGetEntryMeta(entryId);
 
+  /*
+    useGetBranches() => array of branch data => [{name: 'main', sha: '', isProtected: false} ]
+    useGetCurrentBranch() => look up branch in store, return data from useGetBranches with filter => {name:"main", sha: "abc", isProtected: false}
+    useSetBranch(name) => set branch in stor 
+
+  */
+
+  console.log(entryMeta);
+
   const { isLoading, isError, isSuccess, isIdle, isFetching, data, error } =
     useQuery(
-      ["entry_body", [entryMeta?.id, "xyz"]],
-      fetchClient(`/api/content/${entryMeta?.id}/xyz`),
-      { enabled: !!entryMeta?.id }
+      ["entry_body", [entryId, "xyz"]],
+      fetchClient(`/api/content/${entryId}/xyz`),
+      { enabled: !!entryId }
     );
 
   return {
