f0b857be3d0160c474d97165e70cd3d22cc130cf
James Humphreys <BinaryJim@users.noreply.github.com>
2022-03-23 15:52:52 +0000

    Change seed data to object

--- src/mocks/api/seed-data.js
+++ src/mocks/api/seed-data.js
@@ -1,32 +1,28 @@
 import { nanoid } from "nanoid";
 
-export const seedData = [
-  {
-    id: "application/ms_teams",
+export const seedData = {
+  "application/ms_teams": {
     collection: "application",
     meta: {
       title: "Microsoft Teams",
     },
-    content: [
-      {
-        name: "index.md",
+    content: {
+      "index.md": {
         sha: nanoid(),
         content: btoa("I am body content for Microsoft Teams"),
       },
-    ],
+    },
   },
-  {
-    id: "application/ms_outlook",
+  "application/ms_outlook": {
     collection: "application",
     meta: {
       title: "Microsoft Outlook",
     },
-    content: [
-      {
-        name: "index.md",
+    content: {
+      "index.md": {
         sha: nanoid(),
         content: btoa("I am body content for Microsoft Outlook"),
       },
-    ],
+    },
   },
-];
+};
