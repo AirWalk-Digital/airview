eb7e7f57977df6595b5b6a413427e26f582655b4
James Humphreys <BinaryJim@users.noreply.github.com>
2022-03-24 11:55:53 +0000

    Add additional entries to seed-data

--- src/mocks/api/seed-data.js
+++ src/mocks/api/seed-data.js
@@ -36,4 +36,58 @@ export const seedData = {
       },
     },
   },
+  "knowledge/place_call_on_hold": {
+    contentVersion: nanoid(),
+    collection: "knowledge",
+    meta: {
+      title: "Place Call on Hold",
+      parent: "application/ms_teams",
+    },
+    content: {
+      "index.md": {
+        sha: nanoid(),
+        content: btoa(
+          matter.stringify("I am body content for Place Call on Hold", {
+            title: "Place Call on Hold",
+          })
+        ),
+      },
+    },
+  },
+  "knowledge/composing_a_new_message": {
+    contentVersion: nanoid(),
+    collection: "knowledge",
+    meta: {
+      title: "Composing a new message",
+      parent: "application/ms_teams",
+    },
+    content: {
+      "index.md": {
+        sha: nanoid(),
+        content: btoa(
+          matter.stringify("I am body content for Composing a new message", {
+            title: "Composing a new message",
+          })
+        ),
+      },
+    },
+  },
+  "release/security_patch": {
+    contentVersion: nanoid(),
+    collection: "release",
+    meta: {
+      title: "Security Patch",
+      parent: "application/ms_teams",
+    },
+    content: {
+      "index.md": {
+        sha: nanoid(),
+        content: btoa(
+          matter.stringify("I am body content for Security Patch", {
+            title: "Security Patch",
+          })
+        ),
+      },
+    },
+  },
 };
