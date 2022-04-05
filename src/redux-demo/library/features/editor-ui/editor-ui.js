64d07c442ed5fde215eb5ae5cdcdde9145f4b284
James Humphreys <BinaryJim@users.noreply.github.com>
2022-04-05 16:15:37 +0100

    Add confirm dialog if navigating with unsaved changes

--- src/redux-demo/library/features/editor-ui/editor-ui.js
+++ src/redux-demo/library/features/editor-ui/editor-ui.js
@@ -1,7 +1,48 @@
+import { useLayoutEffect } from "react";
+import { useSelector } from "react-redux";
+import { useAirviewRouterHistory } from "../airview-router";
 import { EditorToolbar } from "./editor-toolbar";
 import { MetaEditor } from "./meta-editor";
 
+const isEqual = require("lodash/isEqual");
+
 export function EditorUi({ children }) {
+  const history = useAirviewRouterHistory();
+
+  const { originalData, editsData } = useSelector(
+    (store) => store.editorContext
+  );
+
+  const haveEdits = !isEqual(originalData, editsData);
+
+  useLayoutEffect(() => {
+    const unblock = history.block((tx) => {
+      if (!haveEdits) {
+        unblock();
+        tx.retry();
+      } else {
+        if (
+          window.confirm(
+            "You have unsaved changes; press cancel to keep these changes or ok to dismiss the changes?"
+          )
+        ) {
+          unblock();
+          tx.retry();
+        }
+      }
+    });
+
+    return () => unblock();
+  }, [history, haveEdits]);
+
+  useLayoutEffect(() => {
+    const unlisten = history.listen(() => {
+      console.log("will invalidate branch query");
+    });
+
+    return () => unlisten();
+  }, [history]);
+
   return (
     <div
       style={{ width: "100%", maxWidth: 1200, margin: "0 auto", padding: 16 }}
