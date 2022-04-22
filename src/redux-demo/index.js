392e2032c313cef3e0664d3c8906ca192d0fcc46
James Humphreys <BinaryJim@users.noreply.github.com>
2022-04-22 12:49:27 +0100

    Add proposed project structure

--- src/redux-demo/index.js
+++ src/redux-demo/index.js
@@ -15,3 +15,82 @@ export function ReduxDemo() {
     </AirviewProvider>
   );
 }
+
+/*
+- features
+- - airview-provider
+- - config-provider
+- - custom router
+- - get-remote-branches
+- - get-all-entries-metadata
+- - get-entry
+- - set-editor-context
+- - create-branch
+- - switch-branch
+- - save-edits
+- - clear-edits
+- - raise-pull-request
+- - create-new-content
+- - edit-body-content
+- - edit-metadata
+*/
+
+/*
+- constants
+- util
+- features
+- - redux
+- - - store
+- - - branches
+- - - - fetchRemoteBranches
+- - - - branches-slice
+- - - all-entries-meta
+- - - - fetchAllEntriesMeta
+- - - - all-entries-meta-slice
+- - - editor
+- - - - fetch-entry
+- - - - editor-slice
+- - airview-provider
+- - config-provider
+- - custom router
+- - get-entry
+- - branch-creator
+- - branch-switcher
+- - save-edits
+- - clear-edits
+- - raise-pull-request
+- - content-creator
+- - markdown-editor
+- - metadata-editor
+*/
+
+/*
+- constants
+- util
+- - fetch-client
+- - airview-error
+- features
+- - airview-provider
+- - config-provider
+- - router
+- - editor
+- - - fetch-remote-branches
+- - - editor-slice
+- - - save-edits
+- - - clear-edits
+- - - create-branch
+- - - switch-branch
+- - - raise-pull-request
+- - - create-new-content
+- - - markdown-editor
+- - - metadata-editor
+- - - editor
+- - use-get-all-entries-meta
+- - - fetch-all-entries-meta
+- - - all-entries-meta-slice
+- - use-get-entry
+- - - fetch-entry
+- - - use-get-entry
+- tests
+- stories
+*/
