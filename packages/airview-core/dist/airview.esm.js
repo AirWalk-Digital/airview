058fee720dd083740720736a76df9fe4fac86708
James Humphreys <BinaryJim@users.noreply.github.com>
2022-04-25 10:12:17 +0100

    Rename airview-core dir to airview-cms

--- packages/airview-core/dist/airview.esm.js
+++
@@ -1,8 +0,0 @@
-function greet(name) {
-  return function () {
-    var greeting = name !== null && name !== void 0 ? name : "stranger";
-    console.log("Hello ".concat(greeting, "!"));
-  };
-}
-
-export { greet };
