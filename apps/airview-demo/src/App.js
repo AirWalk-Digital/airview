1a193219c970a0509f5291d549bd29a633b73663
James Humphreys <BinaryJim@users.noreply.github.com>
2022-05-04 15:09:04 +0100

    Working example of chain-reaction data dependencies

--- apps/airview-demo/src/App.js
+++ apps/airview-demo/src/App.js
@@ -1,11 +1,227 @@
 import { AirviewProvider } from "airview-cms";
+import { useState, useEffect, useMemo, useCallback } from "react";
+import matter from "gray-matter";
 
 function App() {
   return (
-    <AirviewProvider>
-      <span>Test</span>
-    </AirviewProvider>
+    // <AirviewProvider>
+    //   <span>Test</span>
+    // </AirviewProvider>
+    <Test />
   );
 }
 
 export default App;
+
+function Test() {
+  const [workingBranch, setWorkingBranch] = useState("main");
+
+  const { state: branchesState, fetchBranches } = useFetchBranches();
+
+  const { state: allEntriesState } = useFetchAllEntriesMeta(
+    branchesState.data,
+    workingBranch
+  );
+
+  const { state: entryState } = useFetchEntry(
+    allEntriesState.data,
+    "release/security_patch"
+  );
+
+  useEffect(() => {
+    fetchBranches();
+  }, [fetchBranches]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("switching branch to 'one'");
+
+      setWorkingBranch("one");
+    }, [5000]);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("refetching branches");
+
+      fetchBranches();
+    }, [8000]);
+  }, [fetchBranches]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      (async () => {
+        console.log("Requesting mutation of entry...");
+
+        const entryData = {
+          content: "Mutated Entry Body",
+          title: "Mutated Entry Title",
+        };
+
+        await fetch(`/api/content/release/security_patch/one`, {
+          method: "PUT",
+          body: JSON.stringify({
+            _index: btoa(
+              matter.stringify(entryData.content, { title: entryData.title })
+            ),
+          }),
+        });
+
+        fetchBranches();
+      })();
+    }, [12000]);
+  }, [fetchBranches]);
+
+  // console.log("branches:", branchesState);
+  // console.log("allEntries:", allEntriesState);
+  // console.log("entry", entryState);
+
+  if (entryState.data) {
+    const body = atob(entryState.data.content["_index"]);
+
+    return <div>{body}</div>;
+  }
+
+  return null;
+}
+
+function useFetchBranches() {
+  const [state, setState] = useState({
+    status: "idle",
+    data: null,
+    error: null,
+  });
+
+  const fetchBranches = useCallback(async () => {
+    try {
+      setState((prevState) => ({
+        ...prevState,
+        status: "loading",
+        error: null,
+      }));
+
+      const response = await fetch("/api/branches");
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+
+      setState((prevState) => ({
+        ...prevState,
+        status: "fulfilled",
+        data,
+      }));
+    } catch (error) {
+      setState((prevState) => ({
+        ...prevState,
+        status: "error",
+        error: error.message,
+      }));
+    }
+  }, []);
+
+  return {
+    state,
+    fetchBranches,
+  };
+}
+
+function useFetchAllEntriesMeta(branches, workingBranch) {
+  const [state, setState] = useState({
+    status: "idle",
+    data: null,
+    error: null,
+  });
+
+  const branchSha = branches?.find(
+    (branch) => branch.name === workingBranch
+  )?.sha;
+
+  const fetchAllEntriesMeta = useCallback(async () => {
+    if (!branchSha) return;
+
+    try {
+      setState((prevState) => ({
+        ...prevState,
+        status: "loading",
+        error: null,
+      }));
+
+      const response = await fetch(`/api/entries/${branchSha}`);
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+
+      setState((prevState) => ({
+        ...prevState,
+        status: "fulfilled",
+        data,
+      }));
+    } catch (error) {
+      setState((prevState) => ({
+        ...prevState,
+        status: "error",
+        error: error.message,
+      }));
+    }
+  }, [branchSha]);
+
+  useEffect(() => {
+    fetchAllEntriesMeta();
+  }, [fetchAllEntriesMeta]);
+
+  return { state };
+}
+
+function useFetchEntry(entriesMeta, entryId) {
+  const [state, setState] = useState({
+    status: "idle",
+    data: null,
+    error: null,
+  });
+
+  const entrySha = entriesMeta?.find((entry) => entry.id === entryId)?.sha;
+
+  const fetchEntry = useCallback(async () => {
+    if (!entrySha) return;
+
+    try {
+      setState((prevState) => ({
+        ...prevState,
+        status: "loading",
+        error: null,
+      }));
+
+      const response = await fetch(`/api/content/${entrySha}`);
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+
+      setState((prevState) => ({
+        ...prevState,
+        status: "fulfilled",
+        data,
+      }));
+    } catch (error) {
+      setState((prevState) => ({
+        ...prevState,
+        status: "error",
+        error: error.message,
+      }));
+    }
+  }, [entrySha]);
+
+  useEffect(() => {
+    fetchEntry();
+  }, [fetchEntry]);
+
+  return { state };
+}
