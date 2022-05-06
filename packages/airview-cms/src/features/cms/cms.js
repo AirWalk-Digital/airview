3cb1c5dd64f0000783d820c576b86e52835bf102
James Humphreys <BinaryJim@users.noreply.github.com>
2022-05-06 16:19:16 +0100

    Add metaEditor and main content components to CMS

--- packages/airview-cms/src/features/cms/cms.js
+++ packages/airview-cms/src/features/cms/cms.js
@@ -1,12 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { ToolBar } from "../toolbar";
 import { BranchCreator } from "../branch-creator";
+import { MetaEditor } from "../meta-editor";
+import { MainContent } from "./main-content";
 
-export function CMS() {
+export function CMS({ children }) {
   return (
     <React.Fragment>
       <ToolBar />
       <BranchCreator />
+      <MetaEditor />
+      <MainContent>{children}</MainContent>
     </React.Fragment>
   );
 }
+
+CMS.propTypes = {
+  children: PropTypes.node,
+};
