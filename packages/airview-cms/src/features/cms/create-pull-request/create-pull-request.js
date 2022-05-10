6473988cedf25c47bc14a32bef00e3fa93ad5239
James Humphreys <BinaryJim@users.noreply.github.com>
2022-05-10 09:29:02 +0100

    Add UI for create pull request component

--- packages/airview-cms/src/features/cms/create-pull-request/create-pull-request.js
+++ packages/airview-cms/src/features/cms/create-pull-request/create-pull-request.js
@@ -9,6 +9,7 @@ import {
   Typography,
   Paper,
 } from "@mui/material";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import { useSelector, useDispatch } from "react-redux";
 import {
   disableCreatePullRequestModal,
@@ -22,26 +23,25 @@ export function CreatePullRequest() {
   const dialogEnabled = useSelector(selectCreatePullRequestModalEnabledStatus);
   const baseBranch = useSelector(selectBaseBranch);
   const workingBranch = useSelector(selectWorkingBranch);
-  const [createPullRequest, { isLoading, isError, error }] =
+  const [createPullRequest, { data, isLoading, isSuccess, isError, reset }] =
     useCreatePullRequestMutation();
 
-  const isValidPr = baseBranch !== workingBranch;
+  const isInvalidPr = baseBranch === workingBranch;
 
-  const handleOnSubmit = async () => {
-    try {
-      await createPullRequest({
-        baseBranch,
-        headBranch: workingBranch,
-      }).unwrap();
-      dispatch(disableCreatePullRequestModal());
-    } catch {
-      return;
-    }
+  const handleOnSubmit = () => {
+    createPullRequest({
+      baseBranch,
+      headBranch: workingBranch,
+    });
   };
 
   const handleOnClose = () => {
     if (isLoading) return;
     dispatch(disableCreatePullRequestModal());
+  };
+
+  const handleOnExited = () => {
+    reset();
   };
 
   return (
@@ -50,55 +50,90 @@ export function CreatePullRequest() {
       fullWidth
       maxWidth="xs"
       onClose={handleOnClose}
+      TransitionProps={{
+        onExited: handleOnExited,
+      }}
     >
       <DialogTitle>Create Pull request</DialogTitle>
+
       <DialogContent dividers>
-        {isError ||
-          (!isValidPr && (
-            <Typography color="error" role="alert" paragraph>
-              {!isValidPr
-                ? "Unable to create pull request, from branch must not equal to branch."
-                : error?.data?.message ??
-                  "Error: Unable to create pull request, please try again"}
+        {isSuccess ? (
+          <React.Fragment>
+            <Typography paragraph>
+              Your pull request has been successfully created, you can view the
+              PR by following the link below.
             </Typography>
-          ))}
 
-        <Paper variant="outlined" sx={{ px: 2, py: 1, mb: 2 }}>
-          <Typography sx={{ lineHeight: 1.2 }}>
-            <Box component="strong" sx={{ display: "block" }}>
-              From:
-            </Box>{" "}
-            {workingBranch}
-          </Typography>
-        </Paper>
+            <Button
+              variant="contained"
+              size="small"
+              disableElevation
+              component="a"
+              href={data}
+              target="_blank"
+              startIcon={<OpenInNewIcon />}
+            >
+              View Pull Request
+            </Button>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            {(isError || isInvalidPr) && (
+              <Typography color="error" role="alert" paragraph>
+                {isInvalidPr
+                  ? 'Unable to create pull request, "from" branch must not equal "to" branch.'
+                  : "Error: Unable to create pull request, please try again"}
+              </Typography>
+            )}
 
-        <Paper variant="outlined" sx={{ px: 2, py: 1 }}>
-          <Typography sx={{ lineHeight: 1.2 }}>
-            <Box component="strong" sx={{ display: "block" }}>
-              To:
-            </Box>{" "}
-            {baseBranch}
-          </Typography>
-        </Paper>
+            <Paper variant="outlined" sx={{ px: 2, py: 1, mb: 2 }}>
+              <Typography sx={{ lineHeight: 1.2 }}>
+                <Box component="strong" sx={{ display: "block" }}>
+                  From:
+                </Box>{" "}
+                {workingBranch}
+              </Typography>
+            </Paper>
+
+            <Paper variant="outlined" sx={{ px: 2, py: 1 }}>
+              <Typography sx={{ lineHeight: 1.2 }}>
+                <Box component="strong" sx={{ display: "block" }}>
+                  To:
+                </Box>{" "}
+                {baseBranch}
+              </Typography>
+            </Paper>
+          </React.Fragment>
+        )}
       </DialogContent>
+
       <DialogActions>
-        <Button
-          variant="outlined"
-          size="small"
-          onClick={handleOnClose}
-          disabled={isLoading}
-        >
-          Cancel
-        </Button>
-        <Button
-          variant="contained"
-          size="small"
-          disableElevation
-          onClick={handleOnSubmit}
-          disabled={isLoading || !isValidPr}
-        >
-          Create
-        </Button>
+        {isSuccess ? (
+          <Button variant="outlined" size="small" onClick={handleOnClose}>
+            Close
+          </Button>
+        ) : (
+          <React.Fragment>
+            <Button
+              variant="outlined"
+              size="small"
+              onClick={handleOnClose}
+              disabled={isLoading}
+            >
+              Cancel
+            </Button>
+
+            <Button
+              variant="contained"
+              size="small"
+              disableElevation
+              onClick={handleOnSubmit}
+              disabled={isLoading || isInvalidPr}
+            >
+              Create
+            </Button>
+          </React.Fragment>
+        )}
       </DialogActions>
     </Dialog>
   );
