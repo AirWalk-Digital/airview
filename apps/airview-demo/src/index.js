3c1f5513415a01d460d6f9302a155f627cd58625
James Humphreys <BinaryJim@users.noreply.github.com>
2022-05-19 15:31:05 +0100

    Adjust MSW response time

--- apps/airview-demo/src/index.js
+++ apps/airview-demo/src/index.js
@@ -9,7 +9,7 @@ import { AirviewMockServer } from "airview-mock-server";
 global.Buffer = global.Buffer || require("buffer").Buffer;
 
 function prepare() {
-  const { handlers } = new AirviewMockServer(1500);
+  const { handlers } = new AirviewMockServer(500);
   const worker = setupWorker(...handlers);
   worker.start();
 
