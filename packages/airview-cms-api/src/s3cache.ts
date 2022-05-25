01a10c96a077cfdfb7aed46abcb415004bd0abc7
Mark Cuckson <59055349+mcuckson@users.noreply.github.com>
2022-05-25 08:47:17 +0100

    Make s3 call proper async

--- packages/airview-cms-api/src/s3cache.ts
+++ packages/airview-cms-api/src/s3cache.ts
@@ -4,43 +4,32 @@ AWS.config.update({ region: "eu-west-1" });
 const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 
 export class S3Cache implements CmsCache {
-  get(key: string): Promise<any> {
-    return new Promise((resolve, reject) => {
-      s3.getObject(
-        {
+  async get(key: string): Promise<any> {
+    try {
+      const file = await s3
+        .getObject({
           Bucket: "msc-demo-s3-bucket",
           Key: key,
-        },
-        function (err, data: any) {
-          if (err) {
-            if (err.code === "NoSuchKey") {
-              resolve(undefined);
-            } else {
-              reject(err);
-            }
-          } else {
-            let objectData = data.Body.toString("utf-8");
-            resolve(JSON.parse(objectData));
-          }
-        }
-      );
-    });
+        })
+        .promise();
+      if (file.Body === undefined) return undefined;
+
+      const objectData = file.Body.toString("utf-8");
+      return JSON.parse(objectData);
+    } catch (err: any) {
+      if (err.code === "NoSuchKey") {
+        return undefined;
+      }
+      throw err;
+    }
   }
-  set(key: string, value: any): Promise<void> {
-    return new Promise((resolve, reject) => {
-      const params = {
-        Body: JSON.stringify(value),
-        Bucket: "msc-demo-s3-bucket",
-        Key: key,
-        ContentType: "application/json",
-      };
-      s3.putObject(params, function (err, data) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    });
+  async set(key: string, value: any): Promise<void> {
+    const params = {
+      Body: JSON.stringify(value),
+      Bucket: "msc-demo-s3-bucket",
+      Key: key,
+      ContentType: "application/json",
+    };
+    await s3.putObject(params).promise();
   }
 }
