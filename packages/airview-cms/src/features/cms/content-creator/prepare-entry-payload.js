8e233d4ffbd3d46fe741ca2d984c7d7f2991d918
James Humphreys <BinaryJim@users.noreply.github.com>
2022-05-27 12:38:36 +0100

    Add support for additional files on entry creation

--- packages/airview-cms/src/features/cms/content-creator/prepare-entry-payload.js
+++ packages/airview-cms/src/features/cms/content-creator/prepare-entry-payload.js
@@ -1,7 +1,7 @@
 import slugify from "slugify";
 import matter from "gray-matter";
 import { selectWorkingBranch } from "../cms.slice";
-//import { selectAllCollections } from "../config-slice";
+import { selectAllCollections } from "../config-slice";
 import {
   selectContentCreatorSelectedCollection,
   selectContentCreatorData,
@@ -11,7 +11,7 @@ export function prepareEntryPayload() {
   return (_, getState) => {
     const state = getState();
     const workingBranch = selectWorkingBranch(state);
-    //const collections = selectAllCollections(state);
+    const collections = selectAllCollections(state);
     const selectedEntryCollection =
       selectContentCreatorSelectedCollection(state);
     const entryMetaData = selectContentCreatorData(state);
@@ -20,11 +20,19 @@ export function prepareEntryPayload() {
       entryMetaData.title
     )}`;
 
+    const { additionalFiles } = collections[selectedEntryCollection];
+
     return {
       id: entryId,
       branch: workingBranch,
       data: {
         _index: btoa(matter.stringify("", entryMetaData)),
+        ...(additionalFiles &&
+          Object.fromEntries(
+            additionalFiles.map((file) => {
+              return [file, btoa(matter.stringify("", {}))];
+            })
+          )),
       },
     };
   };
