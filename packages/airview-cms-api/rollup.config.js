1b24664341e21e3a0f0e0c44e3fee8146f27b908
James Humphreys <BinaryJim@users.noreply.github.com>
2022-06-06 15:37:32 +0100

    Set dependencies as peer dependencies

--- packages/airview-cms-api/rollup.config.js
+++ packages/airview-cms-api/rollup.config.js
@@ -2,14 +2,7 @@ import typescript from "@rollup/plugin-typescript";
 
 export default {
   input: "src/index.ts",
-  external: [
-    "aws-sdk",
-    "gray-matter",
-    "jsonwebtoken",
-    "node-fetch",
-    "sqlite3",
-    "fs",
-  ],
+  external: ["aws-sdk", "gray-matter", "jsonwebtoken", "node-fetch", "fs"],
   output: [{ dir: `dist`, format: "es" }],
   plugins: [typescript()],
 };
