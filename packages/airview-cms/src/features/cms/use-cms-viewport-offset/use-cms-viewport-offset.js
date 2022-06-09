f7c5442b1fe6be65b83471ff216d80c58a0cf92b
James Humphreys <BinaryJim@users.noreply.github.com>
2022-06-09 15:42:42 +0100

    Remove console log

--- packages/airview-cms/src/features/cms/use-cms-viewport-offset/use-cms-viewport-offset.js
+++ packages/airview-cms/src/features/cms/use-cms-viewport-offset/use-cms-viewport-offset.js
@@ -6,7 +6,5 @@ import { TOOL_BAR_HEIGHT } from "../toolbar";
 export function useCMSViewportOffset() {
   const cmsEnabled = useSelector(selectCmsEnabledStatus);
 
-  console.log(cmsEnabled);
-
   return useMemo(() => (cmsEnabled ? TOOL_BAR_HEIGHT : 0), [cmsEnabled]);
 }
