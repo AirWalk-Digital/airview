f958e9701202ad11ea563676d93edcd0c15342e5
James Humphreys <BinaryJim@users.noreply.github.com>
2022-06-14 16:50:12 +0100

    Remove slide transition from NavigationDrawer and add width prop

--- packages/airview-ui/src/features/navigation-drawer/navigation-drawer.js
+++ packages/airview-ui/src/features/navigation-drawer/navigation-drawer.js
@@ -1,30 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Box, Slide } from "@mui/material";
+import { Box } from "@mui/material";
 
-export function NavigationDrawer({ open, top = 0, children }) {
+export function NavigationDrawer({
+  open,
+  top = 0,
+  children,
+  drawerWidth = 300,
+}) {
   return (
-    <Slide in={open} direction="right" timeout={350}>
-      <Box
-        component="aside"
-        sx={{
-          position: "fixed",
-          top,
-          bottom: 0,
-          left: 0,
-          width: 350,
-          borderRight: 1,
-          borderColor: "grey.300",
-          padding: 3,
-          boxSizing: "border-box",
-          backgroundColor: "common.white",
-          zIndex: 1200,
-          overflowY: "auto",
-        }}
-      >
-        {children}
-      </Box>
-    </Slide>
+    <Box
+      component="aside"
+      sx={{
+        display: open ? "block" : "none",
+        position: "fixed",
+        top,
+        bottom: 0,
+        left: 0,
+        width: drawerWidth,
+        borderRight: 1,
+        borderColor: "grey.300",
+        padding: 3,
+        boxSizing: "border-box",
+        backgroundColor: "common.white",
+        zIndex: 1200,
+        overflowY: "auto",
+      }}
+    >
+      {children}
+    </Box>
   );
 }
 
@@ -32,4 +36,5 @@ NavigationDrawer.propTypes = {
   open: PropTypes.bool.isRequired,
   top: PropTypes.number,
   children: PropTypes.node,
+  drawerWidth: PropTypes.number,
 };
