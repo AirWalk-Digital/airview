5715ff782ccaf1151925f1c8a8d2d704d2dc00d1
James Humphreys <BinaryJim@users.noreply.github.com>
2022-06-29 09:47:05 +0100

    Temp disable StyledWysiwyg - conflicting styles

--- apps/airview-demo/src/views/document-view/document-content.js
+++ apps/airview-demo/src/views/document-view/document-content.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { MarkdownEditor } from "airview-cms";
 import { StyledWysiwyg } from "airview-ui/";
-import { TestContent } from "./test-content";
 
 export function DocumentContent({ loading, fetching }) {
   return (
-    <StyledWysiwyg {...{ loading, fetching }}>
-      {/* <TestContent /> */}
-      <MarkdownEditor />
-    </StyledWysiwyg>
+    // <StyledWysiwyg {...{ loading, fetching }}>
+    <MarkdownEditor />
+    // </StyledWysiwyg>
   );
 }
