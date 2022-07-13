4d16a4b440d2a3b2a4e57505e9c2245a1f22f5c7
James Humphreys <BinaryJim@users.noreply.github.com>
2022-07-13 11:49:07 +0100

    Working image resolution outbond

--- packages/airview-cms/src/features/cms/body-editor/markdown-resolver.js
+++ packages/airview-cms/src/features/cms/body-editor/markdown-resolver.js
@@ -21,9 +21,7 @@ export function MarkdownResolverUtils() {
   this.createObjectURLfromBase64String = async function (fileName, string) {
     const blob = await this.convertBase64StringToBlob(fileName, string);
 
-    return {
-      [`${fileName}`]: URL.createObjectURL(blob),
-    };
+    return URL.createObjectURL(blob);
   };
 
   this.convertBase64StringToBlob = async function (fileName, string) {
@@ -77,10 +75,7 @@ export function MarkdownResolverUtils() {
         images[imageURL]
       );
 
-      resolvedImages = {
-        ...resolvedImages,
-        ...image,
-      };
+      resolvedImages[`${imageURL}`] = image;
     }
 
     const resolvedMarkdown = this.findAndReplaceImagesWithinMarkdownString(
@@ -95,46 +90,28 @@ export function MarkdownResolverUtils() {
   };
 
   this.resolveOutbound = async function (markdownBody, images) {
-    console.log(markdownBody);
-    console.log(images);
+    const markdownImageURLs = this.searchMarkdownStringForImages(markdownBody);
 
-    // const markdownImageURLs = this.searchMarkdownStringForImages(markdownBody);
+    let replacements = [];
 
-    // let replacements = [];
+    Object.entries(images).forEach(([key, value]) => {
+      if (markdownImageURLs.includes(value)) {
+        replacements.push({ original: value, replacement: key });
+      }
+    });
 
-    // Object.entries(images).forEach(([key, value]) => {
-    //   if (markdownImageURLs.includes(value)) {
-    //     replacements.push({ original: value, replacement: key });
-    //   }
-    // });
-
-    // const resolvedMarkdown = this.findAndReplaceImagesWithinMarkdownString(
-    //   markdownBody,
-    //   replacements
-    // );
-
-    // let resolvedImages = {};
-
-    // for (const replacement of replacements) {
-    //   console.log(replacement);
-
-    //   resolvedImages[`${replacement.replacement}`] =
-    //     await this.convertObjectURLtoBase64String(replacement.orginal);
-    // }
-
-    const imageString =
-      "iVBORw0KGgoAAAANSUhEUgAAAIwAAABkBAMAAACm+cXiAAAAG1BMVEXMzMyWlpacnJzFxcW3t7e+vr6jo6OxsbGqqqqoPjQzAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAA2ElEQVRYhe3SMQ6CQBAF0GGBQCkBrFU0sRziBcR4AJELyA2INpRUyLFdBhIoASuS/6rNL34ms0MEAAAAAACwboqJYrYO11FwO9IomCTRNTmn93AI7CB5jIIpDI/JzvlL5RC4hVMNwSR2yqRSPtGT6E21BOph7ySYMw5TaXBEsd7KJ5RAsbmRYFaNGRnst3XWtpJAP30JZtW4RT+NXvUf0yjPC7pV7PuaRbsxL3HWtB/jnF/deJVTNfN+SlZgdHcTZ92KF9xNXyNHW+svl2DBFQMAAAAAwOr9AMPbIYfChnnxAAAAAElFTkSuQmCC";
-    console.log("imageString", imageString);
-
-    const imageURL = await this.createObjectURLfromBase64String(
-      "test-image.jpg",
-      imageString
+    const resolvedMarkdown = this.findAndReplaceImagesWithinMarkdownString(
+      markdownBody,
+      replacements
     );
-    console.log("imageURL", imageURL);
 
-    const convertedImageURL = await this.convertObjectURLtoBase64String(
-      imageURL
-    );
-    console.log("convertedImageURL", convertedImageURL);
+    let resolvedImages = {};
+
+    for (const replacement of replacements) {
+      resolvedImages[`${replacement.replacement}`] =
+        await this.convertObjectURLtoBase64String(replacement.original);
+    }
+
+    return { resolvedMarkdown, resolvedImages };
   };
 }
--- packages/airview-cms/src/features/cms/toolbar/save-changes.js
+++ packages/airview-cms/src/features/cms/toolbar/save-changes.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
-//import matter from "gray-matter";
+import matter from "gray-matter";
 import { Button } from "@mui/material";
 import {
   selectDoesMetaEditorHaveEdits,
-  //selectMetaEditorData,
+  selectMetaEditorData,
 } from "../meta-editor";
 import {
   selectDoesBodyEditorHaveEdits,
@@ -13,56 +13,38 @@ import {
   selectInitialImagesData,
   selectEditedImagesData,
 } from "../body-editor";
-import {
-  usePutEntryMutation,
-  //useGetBranchesQuery
-} from "../../store";
-//import { selectCmsContext, selectWorkingBranch } from "../cms.slice";
+import { usePutEntryMutation, useGetBranchesQuery } from "../../store";
+import { selectCmsContext, selectWorkingBranch } from "../cms.slice";
 
 export function SaveChanges() {
   const metaEditorHasEdits = useSelector(selectDoesMetaEditorHaveEdits);
   const bodyEditorHasEdits = useSelector(selectDoesBodyEditorHaveEdits);
-  //const metaEdits = useSelector(selectMetaEditorData);
+  const metaEdits = useSelector(selectMetaEditorData);
   const bodyEdits = useSelector(selectBodyEditorData);
   const initialImagesData = useSelector(selectInitialImagesData);
   const editedImagesData = useSelector(selectEditedImagesData);
-  //const [putEntry, { isLoading }] = usePutEntryMutation();
-  const [{ isLoading }] = usePutEntryMutation();
-  //const id = useSelector(selectCmsContext);
-  //const branch = useSelector(selectWorkingBranch);
-  //const { data: branches } = useGetBranchesQuery();
+  const [putEntry, { isLoading }] = usePutEntryMutation();
+  const id = useSelector(selectCmsContext);
+  const branch = useSelector(selectWorkingBranch);
+  const { data: branches } = useGetBranchesQuery();
 
-  const handleOnClick = () => {
+  const handleOnClick = async () => {
     const resolveMarkdown = new MarkdownResolverUtils();
 
-    resolveMarkdown.resolveOutbound(bodyEdits, {
-      ...initialImagesData,
-      ...editedImagesData,
-    });
+    const { resolvedMarkdown, resolvedImages } =
+      await resolveMarkdown.resolveOutbound(bodyEdits, {
+        ...initialImagesData,
+        ...editedImagesData,
+      });
 
-    return;
-    /*
-      1: Get edited markdown
-      2: search for images
-      3: replace markdown image tags with originals
-      4: spread images into payload
-    */
+    const data = {
+      "_index.md": btoa(matter.stringify(resolvedMarkdown, metaEdits)),
+      ...resolvedImages,
+    };
 
-    /*
-    should have two images objects:
-    - original, pulled from remote
-    - added by user
+    const baseSha = branches.find((f) => f.name === branch).sha;
 
-    The added by user should be purged if a user clears changes
-    The added by user will only be sent in the payload, as in theory other images should already be on the remote
-    */
-    // const data = {
-    //   "_index.md": btoa(matter.stringify(bodyEdits, metaEdits)),
-    // };
-
-    // const baseSha = branches.find((f) => f.name === branch).sha;
-
-    // putEntry({ id, branch, data, baseSha });
+    putEntry({ id, branch, data, baseSha });
   };
 
   return (
