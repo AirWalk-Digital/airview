c372ea8018909086b76090887622e0591e4b9bd9
Mark Cuckson <59055349+mcuckson@users.noreply.github.com>
2022-07-14 12:29:25 +0100

    Fix regressions in demo api

--- apps/airview-api-demo/src/index.ts
+++ apps/airview-api-demo/src/index.ts
@@ -7,20 +7,20 @@ const region = process.env.AWS_S3_REGION;
 if (bucket === undefined) throw Error("No S3 Bucket defined");
 if (region === undefined) throw Error("No S3 Region defined");
 
-const appId = process.env.APP_ID;
-const installationId = process.env.INSTALLATION_ID;
-const privateKeyPath: string = process.env.PRIVATE_KEY_FILE || "";
-const repo = process.env.REPO;
-const org = process.env.ORG;
+const appId = process.env.GITHUB_APP_ID;
+const installationId = process.env.GITHUB_INSTALLATION_ID;
+const privateKeyPath: string = process.env.GITHUB_PRIVATE_KEY_FILE || "";
+const repo = process.env.GITHUB_REPO_NAME;
+const org = process.env.GITHUB_ORG_NAME;
 
-const privateKey = fs.readFileSync(privateKeyPath);
+const privateKey = fs.readFileSync(privateKeyPath, "utf-8");
 
 const client = new GithubClient({
-  applicationId: appId,
-  installationId: installationId,
+  applicationId: appId!,
+  installationId: installationId!,
   privateKey: privateKey,
-  repositoryName: repo,
-  organisation: org,
+  repositoryName: repo!,
+  organisation: org!,
 });
 
 const cache = new S3Cache({
