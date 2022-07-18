5b955d9aee64d8056d0b9a7b2f69c86b74e6b6e9
James Humphreys <BinaryJim@users.noreply.github.com>
2022-07-18 16:09:06 +0100

    Add docs for NavigationDrawer component

--- packages/airview-ui/src/features/navigation-drawer/navigation-drawer.js
+++ packages/airview-ui/src/features/navigation-drawer/navigation-drawer.js
@@ -2,6 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Box } from "@mui/material";
 
+/**
+ *
+ * The NavigationDrawer component is used to display a list of navigation options to a user, within a dismisable aside drawer UI.
+ *
+ * The component accepts children, which will be rendered within the drawer. The visibility of the component is controlled via the `open` prop, the width and top offset can also be customised via the compoent props.
+ *
+ * ## NavigationDrawer States
+ *
+ * The NavigationDrawer can be rendered in two states, namely:
+ *
+ * **Closed - ** The NavigationDrawer is hidden from the user
+ *
+ * **Open - ** The NavigationDrawer is visible to the user
+ *
+ * ## Importing the component
+ *
+ * You can import the NavigationDrawer component as a named import from the airview-ui library
+ *
+ * ```javascript
+ * import { NavigationDrawer } from "airview-ui"
+ * ```
+ */
 export function NavigationDrawer({
   open,
   top = 0,
@@ -34,8 +56,20 @@ export function NavigationDrawer({
 }
 
 NavigationDrawer.propTypes = {
+  /**
+   * Sets the component visibility to the user
+   */
   open: PropTypes.bool.isRequired,
+  /**
+   * Determine the top offset from the viewport
+   */
   top: PropTypes.number,
+  /**
+   * Allows the passing of child nodes, that will will be rendered within the drawer
+   */
   children: PropTypes.node,
+  /**
+   * Sets the width of the drawer UI
+   */
   drawerWidth: PropTypes.number,
 };
---
+++ packages/airview-ui/src/stories/navigation-drawer.stories.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { NavigationDrawer } from "../features";
+
+export default {
+  title: "Components/Navigation Drawer",
+  component: NavigationDrawer,
+  parameters: {
+    docs: {
+      // Opt-out of inline rendering
+      inlineStories: false,
+    },
+  },
+};
+
+const Template = (args) => {
+  return <NavigationDrawer {...args} />;
+};
+
+Template.args = {
+  open: false,
+  children: <span>Navigation Drawer Children</span>,
+};
+
+Template.argTypes = {
+  top: {
+    control: false,
+  },
+  children: {
+    control: false,
+  },
+  drawerWidth: {
+    control: false,
+  },
+};
+
+export const Closed = Template.bind({});
+
+Closed.args = {
+  ...Template.args,
+};
+
+Closed.argTypes = {
+  ...Template.argTypes,
+};
+
+export const Open = Template.bind({});
+
+Open.args = {
+  ...Template.args,
+  open: true,
+};
+
+Open.argTypes = {
+  ...Template.argTypes,
+};
