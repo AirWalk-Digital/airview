630ae88ceb996c4f6c0958776e0c121540235da5
James Humphreys <BinaryJim@users.noreply.github.com>
2022-07-18 12:05:44 +0100

    Instantiate airviewUiThemeProvider in StoryBook global decorators

--- packages/airview-ui/.storybook/preview.js
+++ packages/airview-ui/.storybook/preview.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { AirviewUiThemeProvider } from "../src/features";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +10,11 @@ export const parameters = {
     },
   },
 };
+
+export const decorators = [
+  (Story) => (
+    <AirviewUiThemeProvider>
+      <Story />
+    </AirviewUiThemeProvider>
+  ),
+];
