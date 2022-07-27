68fbaed509d010021ad99d0962ea3490921ab540
James Humphreys <BinaryJim@users.noreply.github.com>
2022-07-27 16:07:57 +0100

    Exclude all node_module imports

--- packages/airview-compliance-ui/rollup.config.js
+++ packages/airview-compliance-ui/rollup.config.js
@@ -2,7 +2,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import json from "@rollup/plugin-json";
 import alias from "@rollup/plugin-alias";
-import commonjs from "@rollup/plugin-commonjs";
 const path = require("path");
 
 const projectRootDir = path.resolve(__dirname);
@@ -10,29 +9,7 @@ const projectRootDir = path.resolve(__dirname);
 export default [
   {
     input: "src/index.js",
-    external: [
-      "react",
-      "@mui/material",
-      //"@mui/lab",
-      "prop-types",
-      "dayjs",
-      //"jss-plugin-compose",
-      "@mui/icons-material/OpenINew",
-      "@mui/material/IconButton",
-      "@mui/icons-material/FilterList",
-      "@mui/material/ListItemIcon",
-      "@mui/icons-material/Settings",
-      "@mui/icons-material/Check",
-      "@mui/icons-material/Clear",
-      "@mui/icons-material/Info",
-      "@mui/icons-material/ExpandMore",
-      "@mui/icons-material/OpenInNew",
-      "@mui/icons-material/Security",
-      "@mui/icons-material/KeyboardArrowDown",
-      "@mui/icons-material/KeyboardArrowUp",
-      "@mui/icons-material/Error",
-      "@mui/icons-material/Warning",
-    ],
+    external: [/node_modules/],
     output: [{ dir: `dist`, format: "es" }],
     plugins: [
       resolve({
@@ -42,7 +19,6 @@ export default [
       }),
       babel({ babelHelpers: "bundled" }),
       json(),
-      commonjs(),
       alias({
         entries: [
           {
