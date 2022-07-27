6c35db234445a7ab391577b5802739e328ae6784
James Humphreys <BinaryJim@users.noreply.github.com>
2022-07-27 15:26:05 +0100

    Fix Message component MUI v5 upgrade errors

--- packages/airview-compliance-ui/src/components/message/message.js
+++ packages/airview-compliance-ui/src/components/message/message.js
@@ -1,42 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
+import { Paper, Typography } from "@mui/material";
 
-const Message = React.forwardRef(
-  ({ borderColor = "#000", title, message, ...rest }, ref) => {
-    return (
-      <Paper
-        variant="outlined"
-        sx={{
-          padding: 2,
-          borderTop: (theme) => `${theme.spacing(0.5)}px solid ${borderColor}`,
-        }}
-        ref={ref}
-        {...rest}
-      >
-        {title && (
-          <Typography align="center" variant="subtitle1">
-            {title}
-          </Typography>
-        )}
+function Message({ borderColor = "#000", title, message, sx, ...rest }) {
+  return (
+    <Paper
+      variant="outlined"
+      sx={{
+        padding: 2,
+        borderTop: 4,
+        borderTopColor: borderColor,
+        ...sx,
+      }}
+      {...rest}
+    >
+      {title && (
+        <Typography align="center" variant="subtitle1">
+          {title}
+        </Typography>
+      )}
 
-        {message && (
-          <Typography align="center" variant="body2">
-            {message}
-          </Typography>
-        )}
-      </Paper>
-    );
-  }
-);
+      {message && (
+        <Typography align="center" variant="body2">
+          {message}
+        </Typography>
+      )}
+    </Paper>
+  );
+}
 
 Message.propTypes = {
   borderColor: PropTypes.string,
   title: PropTypes.string,
   message: PropTypes.string,
+  sx: PropTypes.object,
 };
-
-Message.displayName = "Message";
 
 export { Message };
