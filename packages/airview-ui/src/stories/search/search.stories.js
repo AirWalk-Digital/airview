584e7e0fcd17eb069c5e06b868ab7b11e3977f43
James Humphreys <BinaryJim@users.noreply.github.com>
2022-08-01 12:52:35 +0100

    Update Search story response data to match component API

--- packages/airview-ui/src/stories/search/search.stories.js
+++ packages/airview-ui/src/stories/search/search.stories.js
@@ -25,35 +25,47 @@ const responses = {
       title: "Integer urna ipsum",
       summary:
         "Feugiat ac nulla ac, urna mattis dignissim risus ipsum. Sed feugiat vitae ligula eu pulvinar",
-      path: "/",
+      linkProps: {
+        href: "/",
+      },
     },
     {
       title: "Integer urna ipsum",
-      path: "/",
-    },
-    {
-      title: "Integer urna ipsum",
-      summary:
-        "Feugiat ac nulla ac, urna mattis dignissim risus ipsum. Sed feugiat vitae ligula eu pulvinar",
-      path: "/",
-    },
-    {
-      title: "Integer urna ipsum",
-      summary:
-        "Feugiat ac nulla ac, urna mattis dignissim risus ipsum. Sed feugiat vitae ligula eu pulvinar",
-      path: "/",
+      linkProps: {
+        href: "/",
+      },
     },
     {
       title: "Integer urna ipsum",
       summary:
         "Feugiat ac nulla ac, urna mattis dignissim risus ipsum. Sed feugiat vitae ligula eu pulvinar",
-      path: "/",
+      linkProps: {
+        href: "/",
+      },
     },
     {
       title: "Integer urna ipsum",
       summary:
         "Feugiat ac nulla ac, urna mattis dignissim risus ipsum. Sed feugiat vitae ligula eu pulvinar",
-      path: "/",
+      linkProps: {
+        href: "/",
+      },
+    },
+    {
+      title: "Integer urna ipsum",
+      summary:
+        "Feugiat ac nulla ac, urna mattis dignissim risus ipsum. Sed feugiat vitae ligula eu pulvinar",
+      linkProps: {
+        href: "/",
+      },
+    },
+    {
+      title: "Integer urna ipsum",
+      summary:
+        "Feugiat ac nulla ac, urna mattis dignissim risus ipsum. Sed feugiat vitae ligula eu pulvinar",
+      linkProps: {
+        href: "/",
+      },
     },
   ],
   rejected: { message: "There was an error" },
