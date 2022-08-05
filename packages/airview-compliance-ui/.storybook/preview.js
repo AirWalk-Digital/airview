bf63ba9287b20ef80d7bb50e8f80209164ad7b78
Shenghan Ye <shenghan.ye@airwalkconsulting.com>
2022-08-05 10:17:54 +0100

    story preview refactor

--- packages/airview-compliance-ui/.storybook/preview.js
+++ packages/airview-compliance-ui/.storybook/preview.js
@@ -1,10 +1,5 @@
 import React from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { ThemeProvider } from "@material-ui/core/styles";
-import { makeStyles } from "@material-ui/core/styles";
-import { create } from "jss";
-import { StylesProvider, jssPreset } from "@material-ui/core/styles";
-import compose from "jss-plugin-compose";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -13,16 +8,10 @@ export const parameters = {
 
 export const decorators = [
   (Story) => {
-    const jss = create({
-      plugins: [...jssPreset().plugins, compose()],
-    });
-
     return (
       <React.Fragment>
-        <StylesProvider jss={jss}>
-          <CssBaseline />
-          <Story />
-        </StylesProvider>
+        <CssBaseline />
+        <Story />
       </React.Fragment>
     );
   },
