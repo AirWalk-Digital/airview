8835986bab67667544baf9ce485f9782c3857c35
Jonathan Stockton <jonathan.stockton@airwalkconsulting.com>
2022-08-19 10:39:10 +0100

    Added AWS PR Wrapper

--- deployment/airview-cms-api-aws-lambda-wrapper/src/Api/Cms/index.ts
+++ deployment/airview-cms-api-aws-lambda-wrapper/src/Api/Cms/index.ts
@@ -2,5 +2,6 @@ import * as Branches from "./Branches";
 import * as Entries from "./Entries";
 import * as Content from "./Content";
 import * as Search from "./Search";
+import * as Pulls from "./Pulls";
 
-export { Content, Branches, Entries, Search };
+export { Content, Branches, Entries, Search, Pulls };
