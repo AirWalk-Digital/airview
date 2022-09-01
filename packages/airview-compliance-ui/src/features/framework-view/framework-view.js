f053ff17dedbd09ff13a40c7b6a5fe856b8c22ba
Shenghan Ye <shenghan.ye@airwalkconsulting.com>
2022-09-01 09:01:48 +0100

    framework view component

---
+++ packages/airview-compliance-ui/src/features/framework-view/framework-view.js
@@ -0,0 +1,136 @@
+import React, { useMemo } from "react";
+import PropTypes from "prop-types";
+import { useTheme } from "@mui/material/styles";
+import {
+  Paper,
+  Table,
+  TableContainer,
+  TableBody,
+  Skeleton,
+  Typography,
+} from "@mui/material";
+import { FrameworkViewToolbar } from "./framework-view-toolbar";
+import { FrameworkViewHeader } from "./framework-view-header";
+import { FrameworkViewRow } from "./framework-view-row";
+import { FrameworkViewRowDetail } from "./framework-view-row-detail";
+import { Message } from "../../components";
+
+export function FrameworkView({
+  title,
+  summary,
+  applications,
+  loading,
+  noDataMessage,
+  invalidPermissionsMessage,
+}) {
+  const theme = useTheme();
+
+  const getProcessedApplicationData = useMemo(() => {
+    if (loading || !applications) return;
+
+    return applications;
+  }, [applications, loading]);
+
+  if (loading) {
+    return <Skeleton variant="rect" width="100%" height={200} />;
+  }
+
+  if (!applications) {
+    return (
+      <Message
+        borderColor={theme.palette.primary.main}
+        title={invalidPermissionsMessage.title}
+        message={invalidPermissionsMessage.message}
+      />
+    );
+  }
+
+  if (applications.length < 1) {
+    return (
+      <Message
+        borderColor={theme.palette.primary.main}
+        title={noDataMessage.title}
+        message={noDataMessage.message}
+      />
+    );
+  }
+
+  return (
+    <TableContainer component={Paper}>
+      <FrameworkViewToolbar {...{ title }} />
+
+      <Typography variant="h6" sx={{ marginLeft: 2 }}>
+        Summary
+      </Typography>
+
+      <Typography variant="body2" sx={{ marginLeft: 2 }}>
+        {summary}
+      </Typography>
+
+      <Table arial-label="Compliance table">
+        <FrameworkViewHeader />
+
+        <TableBody>
+          {getProcessedApplicationData.map((application) => (
+            <FrameworkViewRow key={application.id} {...application}>
+              <FrameworkViewRowDetail
+                detailData={application.applicationDetailData}
+              />
+            </FrameworkViewRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+FrameworkView.propTypes = {
+  /**
+   * Presents the component in a lodaing state (for when fetching data async)
+   */
+  loading: PropTypes.bool,
+  /**
+   * An optional title for the table
+   */
+  title: PropTypes.string,
+  /**
+   * A summary for the table
+   */
+  summary: PropTypes.string,
+  /**
+   * The collection of applications to render to the complianceTable, should be an array of applications, an empty array (for no issues) of null (for invalid permissions)
+   */
+  applications: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      controlID: PropTypes.string.isRequired,
+      controlTitle: PropTypes.string.isRequired,
+      owner: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      applicationDetailData: PropTypes.shape({
+        detail: PropTypes.string,
+        controlMappings: PropTypes.arrayOf(
+          PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            gap: PropTypes.string.isRequired,
+          })
+        ),
+        Frequency: PropTypes.string,
+      }),
+    })
+  ),
+  /**
+   * Used to display a message to the user when they do not have required permissions to view the application data
+   */
+  invalidPermissionsMessage: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    message: PropTypes.string.isRequired,
+  }).isRequired,
+  /**
+   * Used to display a message to the user when there is no data to display for the application
+   */
+  noDataMessage: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    message: PropTypes.string.isRequired,
+  }).isRequired,
+};
