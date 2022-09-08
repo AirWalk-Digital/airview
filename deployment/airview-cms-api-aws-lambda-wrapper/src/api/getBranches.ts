c1e38cc7f9830b7c906fc9339e59fb5a7d0db6e7
Mark Cuckson <59055349+mcuckson@users.noreply.github.com>
2022-09-08 09:20:47 +0100

    Remove comments

--- deployment/airview-cms-api-aws-lambda-wrapper/src/api/getBranches.ts
+++ deployment/airview-cms-api-aws-lambda-wrapper/src/api/getBranches.ts
@@ -18,30 +18,3 @@ export function getBranches() {
 
   return { handle };
 }
-/*
-export class getBranches extends CmsApiHandler {
-  public async process(request: Request, response: Response): Promise<void> {
-    let data: any;
-    try {
-      data = await this.cmsBackend.getBranches();
-      Utilities.Functions.print_debug(`Data: ${data}`);
-    } catch (error) {
-      console.log(error);
-      this.response.fail(
-        new Errors.InternalServerError({
-          Function: this.context.functionName,
-          Name: this.context.logStreamName,
-          Request: this.context.awsRequestId,
-        })
-      );
-    }
-
-    response
-      .setBody(JSON.stringify(data))
-      .addHeader("content-type", "application/json")
-      .send();
-    return Promise.resolve();
-  }
-}
-
-*/
--- deployment/airview-cms-api-aws-lambda-wrapper/src/api/getEntryBySha.ts
+++ deployment/airview-cms-api-aws-lambda-wrapper/src/api/getEntryBySha.ts
@@ -21,30 +21,3 @@ export function getEntryBySha() {
 
   return { handle };
 }
-
-/*
-export class getEntryBySha extends CmsApiHandler {
-  public async process(request: Request, response: Response): Promise<void> {
-    let data: any;
-    try {
-      data = await this.cmsBackend.getEntries(request.getPathParameter("sha"));
-      Utilities.Functions.print_debug(`Data: ${data}`);
-    } catch (error) {
-      console.log(error);
-      this.response.fail(
-        new Errors.InternalServerError({
-          Function: this.context.functionName,
-          Name: this.context.logStreamName,
-          Request: this.context.awsRequestId,
-        })
-      );
-    }
-
-    response
-      .setBody(JSON.stringify(data))
-      .addHeader("content-type", "application/json")
-      .send();
-    return Promise.resolve();
-  }
-}
-*/
