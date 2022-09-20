8c1e7c525d4314a854bd6ac90310c127768c13ae
James Humphreys <BinaryJim@users.noreply.github.com>
2022-09-20 12:35:18 +0100

    #294: Remove margin and padding rules for print

--- packages/airview-ui/src/features/aside-and-main/aside-and-main.js
+++ packages/airview-ui/src/features/aside-and-main/aside-and-main.js
@@ -6,7 +6,15 @@ export function AsideAndMainContainer({ children, sx, ...rest }) {
   return (
     <MuiContainer
       maxWidth={false}
-      sx={{ paddingTop: 6, paddingBottom: 6, ...sx }}
+      sx={{
+        paddingTop: 6,
+        paddingBottom: 6,
+        "@media print": {
+          margin: 0,
+          padding: 0,
+        },
+        ...sx,
+      }}
       {...rest}
     >
       <Box sx={{ display: "flex" }}>{children}</Box>
