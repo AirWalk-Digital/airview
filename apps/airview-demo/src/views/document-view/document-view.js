154284b363a35b54e553c3fce121e1d39405f552
James Humphreys <BinaryJim@users.noreply.github.com>
2022-10-18 10:42:07 +0100

    294 add print view (#101)

--- apps/airview-demo/src/views/document-view/document-view.js
+++ apps/airview-demo/src/views/document-view/document-view.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Navigate, Link as ReactRouterLink } from "react-router-dom";
 import { useSetCmsContext } from "airview-cms";
 import {
@@ -8,13 +8,21 @@ import {
   Main,
   Breadcrumb,
 } from "airview-ui";
+import { useHtmlToPdfUtil } from "airview-html-to-pdf-util";
 import { useGetBreadcrumbLinksData } from "./use-get-breadcrumb-links-data";
 import { useGetEntryId } from "./use-get-entry-id";
 import { TableOfContents } from "./table-of-contents";
 import { DocumentContent } from "./document-content";
+import { DownloadDocumentPdfButton } from "./download-document-pdf";
+
+/* eslint import/no-webpack-loader-syntax: off */
+import css from "!!raw-loader!../../print.css";
 
 export function DocumentView() {
   const { entryId, path } = useGetEntryId();
+  const { print, ...status } = useHtmlToPdfUtil();
+
+  const contentsRef = useRef();
 
   const { data, isError, error, isUninitialized, isLoading, isFetching } =
     useSetCmsContext({ entryId, path });
@@ -36,15 +44,26 @@ export function DocumentView() {
           sx={{ marginBottom: 4 }}
           linkComponent={ReactRouterLink}
         />
-        <PageTitle
-          title={data?.title ?? ""}
-          loading={isLoading || isUninitialized}
-          fetching={isFetching}
-        />
-        <DocumentContent
-          loading={isLoading || isUninitialized}
-          fetching={isFetching}
-        />
+        <div>
+          <DownloadDocumentPdfButton
+            onClick={() => {
+              print(contentsRef?.current?.innerHTML, css);
+            }}
+            status={status}
+            disabled={isUninitialized || isLoading || isFetching || isError}
+          />
+          <div ref={contentsRef}>
+            <PageTitle
+              title={data?.title ?? ""}
+              loading={isLoading || isUninitialized}
+              fetching={isFetching}
+            />
+            <DocumentContent
+              loading={isLoading || isUninitialized}
+              fetching={isFetching}
+            />
+          </div>
+        </div>
       </Main>
       <Aside>
         <TableOfContents />
