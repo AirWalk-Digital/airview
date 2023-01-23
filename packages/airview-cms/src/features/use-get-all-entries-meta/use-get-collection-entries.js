291bdddb1e0740fded55e8895bc9a72b1301b6c6
Mark Cuckson <59055349+mcuckson@users.noreply.github.com>
2023-01-23 16:14:50 +0000

    Remove commented lines

--- packages/airview-cms/src/features/use-get-all-entries-meta/use-get-collection-entries.js
+++ packages/airview-cms/src/features/use-get-all-entries-meta/use-get-collection-entries.js
@@ -1,25 +1,10 @@
 import { useGetAllEntriesMeta } from "./use-get-all-entries-meta";
 
 export function useGetCollectionEntries(collectionId) {
-  //  const values = [];
-
   const findCollectionEntries = (entries) => {
     if (!entries) return;
 
     return entries[collectionId];
-    /*
-    const collectionEntries = entries.filter(
-      (entry) => entry.collection === collectionId
-    );
-
-    return collectionEntries.length
-      ? (() => {
-          values.length = 0;
-          values.push(...collectionEntries);
-          return values;
-        })()
-      : undefined;
-      */
   };
 
   return useGetAllEntriesMeta(
