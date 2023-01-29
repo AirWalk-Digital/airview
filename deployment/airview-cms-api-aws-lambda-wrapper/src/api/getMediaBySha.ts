2b2c7c85b64fd08f3a569a74e0b8065506556747
Mark Cuckson <59055349+mcuckson@users.noreply.github.com>
2023-01-29 12:01:24 +0000

    Rewire lambda to backend

--- deployment/airview-cms-api-aws-lambda-wrapper/src/api/getMediaBySha.ts
+++ deployment/airview-cms-api-aws-lambda-wrapper/src/api/getMediaBySha.ts
@@ -1,5 +1,6 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from "aws-lambda";
 import { cmsBackendFactory, utils } from "../common";
+import { fileTypeFromBuffer } from "file-type";
 
 export function getMediaBySha() {
   const factory = cmsBackendFactory();
@@ -7,18 +8,18 @@ export function getMediaBySha() {
     event: APIGatewayEvent,
     context: Context
   ): Promise<APIGatewayProxyResult> {
-    let data: any;
-    const path: string = event.queryStringParameters["path"];
     const sha: string = event.pathParameters["sha"];
-    utils.printDebug(`Query String: path == "${path}"`);
     utils.printDebug(`Query String: sha == "${sha}"`);
 
     const cmsBackend = await factory.getInstance();
-    data = await cmsBackend.getTreeContent(sha, path);
+    const data = await cmsBackend.getContent(sha);
+    const buffer = Buffer.from(data.content, "base64");
+    const contentType = await fileTypeFromBuffer(buffer);
     return {
       statusCode: 200,
       body: data.content,
       isBase64Encoded: true,
+      headers: { "content-type": contentType?.mime || "text/plain" },
     };
   }
 
